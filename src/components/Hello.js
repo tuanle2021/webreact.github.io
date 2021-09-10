5ad3fdead69c479c7e36289b8008f37a2990eaa5
tuanle2021 <tuanle2021>
2021-09-10 19:06:36 +0700

    web lan 1

---
+++ src/components/Hello.js
@@ -0,0 +1,25 @@
+function Hello() {
+  return (
+    <div>
+    <div className="cate">
+        <div className="row">
+          <div className="col-xs-6 col-md-3">
+            <a href="#" className="thumbnail">
+              <img src="./img/img1.jpg" alt="..."/>
+            </a>
+          </div>
+          ...
+        </div>
+
+    </div>
+      <nav aria-label="...">
+          <ul className="pager">
+            <li><a href="#">Previous</a></li>
+            <li><a href="#">Next</a></li>
+          </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Hello;
